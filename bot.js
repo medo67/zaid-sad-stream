d9ca954927233702dda8d7e19a9b376936572e88
medo67 <45091642+medo67@users.noreply.github.com>
2019-04-19 20:55:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,12 +22,7 @@ client.on('message', message => {
   if (message.content.startsWith(adminprefix + 'lis')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**Status You  ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
-      message.channel.send(`**Status You ${argresult} **`)
-}
-});
+
 
 
 
